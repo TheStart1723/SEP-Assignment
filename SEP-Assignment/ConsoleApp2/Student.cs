bc437c762252551aa67a5845b9ab19cbfa3ff3ad
TheStart1723 <791058145@qq.com>
2021-11-08 21:36:48 -0600

    11/4

---
+++ SEP-Assignment/ConsoleApp2/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp2
+{
+    public abstract class Student
+    {
+        public Student()
+        {
+
+        }
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Major { get; set; }
+        public abstract void TakeClass();
+    }
+}
